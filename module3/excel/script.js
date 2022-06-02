0ac99d9d8737fdecb29b74fa2a402ce3eecf5297
preet-design <100865792+preet-design@users.noreply.github.com>
2022-06-02 22:14:54 +0530

    util

--- module3/excel/script.js
+++ module3/excel/script.js
@@ -5,6 +5,9 @@ let leftCol = document.querySelector(".left-col");
 let topLeftCell = document.querySelector(".top-left-cell");
 let allCells = document.querySelectorAll(".cell");
 let addressInput = document.querySelector("#address");
+let formulaInput = document.querySelector("#formula");
+let lastSelectedCell;
+
 
 cellsContentDiv.addEventListener("scroll",function(e){
     let scrollFromTop = e.target.scrollTop;
@@ -14,17 +17,20 @@ cellsContentDiv.addEventListener("scroll",function(e){
     topLeftCell.style.top = scrollFromTop+"px";
     topLeftCell.style.left = scrollFromLeft+"px";
 })
-
 for(let i=0;i<allCells.length;i++){
     allCells[i].addEventListener("click",function(e){
         let rowId = Number(e.target.getAttribute("rowid"));
         let colId = Number(e.target.getAttribute("colid"));
         let address = String.fromCharCode(65+colId)+(rowId+1)+"";
         // console.log(address);
+        let cellObject = db[rowId][colId];
         addressInput.value = address;
+        //update UI
+        formulaInput.value = cellObject.formula;
     })
 
     allCells[i].addEventListener("blur",function(e){
+        lastSelectedCell = e.target;
         let cellValue = e.target.textContent;
         let {rowId,colId} = getRowIdColIdFromElement(e.target);
         let cellObject = db[rowId][colId];
@@ -36,13 +42,17 @@ for(let i=0;i<allCells.length;i++){
     })
 }
 
-function getRowIdColIdFromElement(element){
-    let rowId  = element.getAttribute("rowid");
-    let colId = element.getAttribute("colid");
-    return {
-        rowId,
-        colId
+
+formulaInput.addEventListener("blur",function(e){
+    let formula = e.target.value;
+    if(formula){
+        let {rowId,colId} = getRowIdColIdFromElement(lastSelectedCell);
+        let cellObject = db[rowId][colId];
+        let computedValue = solveFormula(formula , cellObject); // will implement in next commit
+        //update db
+        cellObject.value = computedValue;
+        cellObject.formula = formula;
+        //update ui
+        lastSelectedCell.textContent = computedValue;
     }
-}
-
-
+})
